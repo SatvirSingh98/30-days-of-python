5bed94be8a458437f6264cfaba4e0585231681f4
satvir <satvirsingh.juc@gmail.com>
2021-02-17 13:25:17 +0530

    improved and can be used as module and return json like data

--- Day 4-Send email/inbox.py
+++ Day 4-Send email/inbox.py
@@ -7,29 +7,43 @@ from decouple import config
 username = config('username')
 password = config('password')
 
-mail = imaplib.IMAP4_SSL(host='imap.gmail.com')
-mail.login(user=username, password=password)
-mail.select('inbox')
 
-_, search_data = mail.search(None, 'ALL')
+def get_inbox():
+    mail = imaplib.IMAP4_SSL(host='imap.gmail.com')
+    mail.login(user=username, password=password)
+    mail.select('inbox')
 
-for num in search_data[0].split():
-    _, data = mail.fetch(num, '(RFC822)')
-    # print(data[0])
-    _, b = data[0]
-    email_message = email.message_from_bytes(b)
-    # print(email_message)
+    _, search_data = mail.search(None, 'ALL')
+    my_messages = []
 
-    for header in ['subject', 'to', 'from', 'date']:
-        print(f'{header}: {email_message[header]}')
+    for num in search_data[0].split():
+        email_data = {}
+        _, data = mail.fetch(num, '(RFC822)')
+        # print(data[0])
+        _, b = data[0]
+        email_message = email.message_from_bytes(b)
+        # print(email_message)
 
-    for part in email_message.walk():
-        if part.get_content_type() == 'text/plain':
-            body = part.get_payload(decode=True)
-            print(body.decode(), '\n')
+        for header in ['subject', 'to', 'from', 'date']:
+            print(f'{header}: {email_message[header]}')
+            email_data[header] = email_message[header]
 
-        elif part.get_content_type() == 'text/html':
-            html_body = part.get_payload(decode=True)
-            print(html_body.decode(), '\n')
+        for part in email_message.walk():
+            if part.get_content_type() == 'text/plain':
+                body = part.get_payload(decode=True)
+                print(body.decode(), '\n')
+                email_data['body'] = body.decode()
 
-mail.close()
+            elif part.get_content_type() == 'text/html':
+                html_body = part.get_payload(decode=True)
+                print(html_body.decode(), '\n')
+                email_data['body'] = html_body.decode()
+
+    my_messages.append(email_data)
+    mail.close()
+    return my_messages
+
+
+if __name__ == "__main__":
+    my_inbox = get_inbox()
+    print(my_inbox)
