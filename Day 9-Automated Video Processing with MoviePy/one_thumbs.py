2f7e9671e0270796bb8d6826afc1775e49b98eec
satvir <satvirsingh.juc@gmail.com>
2021-03-01 14:14:12 +0530

    Automating video processing and making thumnail for frame per sec.

---
+++ Day 9-Automated Video Processing with MoviePy/one_thumbs.py
@@ -0,0 +1,36 @@
+import os
+
+from moviepy.editor import VideoFileClip
+from PIL import Image
+
+from conf import SAMPLE_INPUTS, SAMPLE_OUTPUTS
+
+
+source_path = os.path.join(SAMPLE_INPUTS, 'sample.mp4')
+thumbnail_dir = os.path.join(SAMPLE_OUTPUTS, 'thumbnails')
+
+os.makedirs(thumbnail_dir, exist_ok=True)
+
+
+clip = VideoFileClip(source_path)
+print(clip.reader.fps)  # number of frames per second = 30.0
+print(clip.reader.nframes)  # total number of frames = 906
+print(clip.duration)  # seconds = 30.17
+
+fps = clip.reader.fps
+nframes = clip.reader.nframes
+sec = nframes / (fps * 1.0)  # 1.0 is multiplied to prevent round-off error
+# or sec = clip.duration
+
+# now we have to create image for every frame
+duration = int(clip.duration)  # or clip.reader.duration
+
+
+# thumbnail for frame per second
+for i in range(duration):
+    frame = clip.get_frame(i)
+    # print(frame, '\n') numpy array of pixels at particular second of clip
+    img_path = os.path.join(thumbnail_dir, f"{i}.jpg")
+    # print(f'frame at {i+1} seconds at {new_img_path}')
+    img = Image.fromarray(frame)
+    img.save(img_path)
