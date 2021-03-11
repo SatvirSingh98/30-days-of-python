3bfbe04214990b6f3c1935a76e4d900af6570723
satvir <satvirsingh.juc@gmail.com>
2021-03-11 23:46:31 +0530

    shifted from virtualenv to pipenv

--- Day 10-Scrape & automate behind password protected apps/conf.py
+++ Day 10-Scrape & automate behind password protected apps/conf.py
@@ -2,4 +2,4 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CHROMEDRIVER_DIR = os.path.join(BASE_DIR, '.chromedriver')
-CHROMEDRIVER_PATH = os.path.join(GECKODRIVER_DIR, 'chromedriver')
+CHROMEDRIVER_PATH = os.path.join(CHROMEDRIVER_DIR, 'chromedriver')
